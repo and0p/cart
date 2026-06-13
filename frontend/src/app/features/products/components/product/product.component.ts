4a1328170c59249fb8fc8585691cb0dade244f29
Andrew Peterson <andrew@peterson.sh>
2026-06-13 16:36:38 -0400

    Small changes to services and components for products and carts

--- frontend/src/app/features/products/components/product/product.component.ts
+++ frontend/src/app/features/products/components/product/product.component.ts
@@ -1,13 +1,38 @@
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, computed, CUSTOM_ELEMENTS_SCHEMA, input, signal } from "@angular/core";
+
 import { Product } from "../../types/products.type";
+
+import { MatCardContent, MatCardHeader, MatCardModule, MatCardSubtitle, MatCardTitle, MatCardImage } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatCardModule, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardImage, MatButtonModule, MatInputModule, MatFormFieldModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ProductComponent {
-    product = input<Product>();
+  product = input<Product>();
+
+  amount = signal<number>(0);
+
+  test = computed(() => {
+    console.log(this.amount());
+  })
+
+  incrementAmount = () => {
+    this.amount.update(current => current + 1);
+  }
+
+  decrementAmount = () => {
+    this.amount.update(current => current - 1);
+  }
+
+  buy = () => {
+    // TODO cart store add by current amount
+  }
 }
--- frontend/src/app/features/products/pages/catalog/catalog.component.ts
+++ frontend/src/app/features/products/pages/catalog/catalog.component.ts
@@ -1,13 +1,16 @@
 import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject } from "@angular/core";
 import { ProductService } from "../../services/products.service";
 import { ProductStore } from "../../store/products.store";
+import { ProductComponent } from "../../components/product/product.component";
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'catalog',
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [ProductComponent, MatGridListModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CatalogComponent {
   productService = inject(ProductService);
